7252da031072ef51fbed9cb695d87db32ba8961b
Faisal Mohammed <m.faisal244@gmail.com>
2024-06-19 13:47:30 +0100

    Implemented an endpoint for listing all cars

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
 import express, { Express, Request, Response } from "express";
+import VehicleRepository from "./repositories/vehicle-repository";
 
 const app: Express = express();
 const port = 3000;
 
+const vehicleRepo = new VehicleRepository();
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
+});
+
+// Endpoint to list all cars
+app.get("/cars", (req, res) => {
+  try {
+    const vehicles = vehicleRepo.getAll();
+    res.json(vehicles);
+  } catch (error) {
+    res.status(500).send("Error retrieving vehicles");
+  }
 });
 
 app.listen(port, () => {
